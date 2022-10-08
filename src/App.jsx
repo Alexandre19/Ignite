6bf6c3b8f7144ab97553fcea27fddba7d95e4720
Alexandre Borges <alexandreborges.goncalves@gmail.com>
2022-10-08 17:24:14 -0300

    Estrutura da aplicação - CSS global

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Header } from "./components/Header"
+import { Post } from "./Post"
+
+import './global.css'
+
+export function App() {
+
+  return (
+    <div>
+       
+       <Header />
+
+      <Post author="Alexandre Borges"
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, harum alias. Error in voluptatum recusandae veniam fuga deleniti libero maxime voluptatibus nulla perspiciatis, voluptatem quas, ad autem temporibus totam distinctio!"
+      />
+    
+    </div>
+  )
+}
+
+
+
+
